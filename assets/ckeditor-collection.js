3f15ef299848119d0f3dc08cb3cadb7498fa6951
Parveen Sangroya <9700282+sangroya@users.noreply.github.com>
2020-12-29 19:02:04 +0530

    Update ckeditor-collection.js

--- assets/ckeditor-collection.js
+++ assets/ckeditor-collection.js
@@ -1,1 +1,8 @@
 let CKEditor= new Map();
+CKEditor.insertText=(element,content)=>{
+   
+    var editor=CKEditor.get(element);
+    var viewFragment = editor.data.processor.toView( content );
+    var modelFragment = editor.data.toModel( viewFragment );
+    editor.model.insertContent( modelFragment );
+};
